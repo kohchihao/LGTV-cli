661e632ada2d523e70d197b79ec8d2eed99a01bb
Marcus Koh <kohchihao@gmail.com>
2019-02-03 19:51:48 +0800

    app.js: Update to use only long keyword

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ const cli = meow(`
         }
     }
 });
+
 const action = Object.keys(cli.flags)[0];
 console.log(action);
 var lgtv = require('lgtv2')({
@@ -42,25 +43,21 @@ lgtv.on('error', function(err) {
 lgtv.on('connect', function() {
   console.log('connected to tv.');
 
-  // lgtv.subscribe('ssap://audio/getVolume', function(err, res) {
-  //   if (res.changed.indexOf('volume') !== -1)
-  //     console.log('volume changed', res.volume);
-  //   if (res.changed.indexOf('muted') !== -1)
-  //     console.log('mute changed', res.muted);
-  // });
-  
   switch (action) {
-    case 'setvolume','sv':
+    case 'sv':
+    case 'setvolume':
       lgtv.request('ssap://audio/setVolume', { volume: parseInt(cli.flags.setvolume) });
       console.log("Volume changed to " + cli.flags.setvolume);
       lgtv.disconnect();
       break;
-    case 'turnoff','to':
+    case 'to':
+    case 'turnoff':
       lgtv.request('ssap://system/turnOff', function(err, res) {
         lgtv.disconnect();
       });
       break;
-    case 'message','m':
+    case 'm':
+    case 'message':
       lgtv.request('ssap://system.notifications/createToast', {
         message: cli.flags.message
       });
